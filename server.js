a927e949c00ebe14719fb32d26497f379852981b
伍子轩(zixuan) <101704207+de4O4@users.noreply.github.com>
2025-05-31 15:51:11 +0800

    验证服务器

---
+++ server.js
@@ -0,0 +1,45 @@
+const grpc = require('@grpc/grpc-js')
+const message_proto = require('./proto')
+const const_module = require('./const')
+const { v4: uuidv4 } = require('uuid');
+const emailModule = require('./email');
+const config_module = require("./config.js");
+
+
+
+
+async function GetVarifyCode(call, callback) {
+    console.log("email is ", call.request.email)
+    //console.log('加载的邮箱配置 - user:', config_module.email_user);
+    //console.log('加载的邮箱配置 - pass:', config_module.email_pass ? '*****' : '未获取到授权码');
+    try{
+        uniqueId = uuidv4();
+        console.log("uniqueId is ", uniqueId)
+        let text_str =  '您的验证码为'+ uniqueId +'请三分钟内完成注册'
+        //发送邮件
+        let mailOptions = {
+            from: '1195525350@qq.com',
+            to: call.request.email,
+            subject: '验证码',
+            text: text_str,
+        };
+        let send_res = await emailModule.SendMail(mailOptions);
+        console.log("send res is ", send_res)
+        callback(null, { email:  call.request.email,
+            error:const_module.Errors.Success
+        }); 
+    }catch(error){
+        console.log("catch error is ", error)
+        callback(null, { email:  call.request.email,
+            error:const_module.Errors.Exception
+        }); 
+    }
+}
+function main() {
+    var server = new grpc.Server()
+    server.addService(message_proto.VarifyService.service, { GetVarifyCode: GetVarifyCode })
+    server.bindAsync('0.0.0.0:50052', grpc.ServerCredentials.createInsecure(), () => {
+        console.log('grpc server started')        
+    })
+}
+main()
