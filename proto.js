fb5331542f961d47db408e03aed83bd0ab4349f0
伍子轩(zixuan) <101704207+de4O4@users.noreply.github.com>
2025-06-16 23:04:33 +0800

    Delete proto.js

--- proto.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')    /*将 .proto 文件中的协议定义（服务、消息、枚举等）转换为 JavaScript 对象8*/
-const grpc = require('@grpc/grpc-js')
-const protoLoader = require('@grpc/proto-loader')
-const PROTO_PATH = path.join(__dirname, 'message.proto')
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true })
-const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
-const message_proto = protoDescriptor.message
-module.exports = message_proto
